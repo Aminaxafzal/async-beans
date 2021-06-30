d31e3ea0bad2c975dafb7c107d57253f9d85a13d
minamedia <mina.m3dia@gmail.com>
2021-06-30 15:32:17 +0100

    independent-promises

--- main.js
+++ main.js
@@ -38,3 +38,23 @@ console.log('Ordering a pizza!');
 
 
 hostDinnerParty();
+
+
+
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+
+// Write your code below:
+
+
+
+async function serveDinner() {
+ const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice(); 
+  const proteinPromise = bakeChicken(); 
+  const sidePromise = cookBeans(); 
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+  
+}
+
+
+serveDinner();
