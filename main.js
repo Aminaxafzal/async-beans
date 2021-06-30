bd5060afd79f848d264da48ea43af42e13606de9
minamedia <mina.m3dia@gmail.com>
2021-06-30 14:47:58 +0100

    dependent-promises-final

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+
+// In the following checkpoints, I will create an async function that handles the 
+// three dependent promises we just walked through. It will simulate shopping for, 
+// soaking, and then cooking the beans. To get started, i will declare an async function, 
+// makeBeans(). 
+
+// Write your code below:
+async function makeBeans() {
+    let type = await shopForBeans(); 
+    console.log(type);
+    let isSoft = await soakTheBeans(type);
+    console.log(isSoft);
+    let dinner = await cookTheBeans(isSoft); 
+  console.log(dinner);
+  
+  }
+  
+  makeBeans(); 
